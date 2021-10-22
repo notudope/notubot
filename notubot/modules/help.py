3586864c70042f0779cfe4fc5bec30f57ebdcb38
notudope <notudope@gmail.com>
2021-10-22 18:06:59 +0700

    update

--- notubot/modules/help.py
+++ notubot/modules/help.py
@@ -25,43 +25,37 @@ async def help(event):
         if args in CMD_HELP:
             await event.edit(f"Module **{CMD_HELP[args][0]}**:\n\n" + str(CMD_HELP[args][1]))
         else:
-            await event.edit(f"😖 Module [`{args}`] tidak ada! Ketik ```.help``` untuk melihat nama module yang benar.")
+            await event.edit(f"😐 Module [`{args}`] tidak ada! Ketik ```.help``` untuk melihat nama module yang benar.")
             await asyncio.sleep(200)
             await event.delete()
     else:
-        head = f"`{BOT_NAME}`"
-        head1 = f"[REPO](https://github.com/notudope/notubot)  /  [Channel](https://t.me/notudope)  /  [Grup](https://t.me/NOTUBOTS)"
-
-        head2 = f"😎 **Owner :** __{ALIVE_NAME}__"
-        head3 = f"🤖 **Version :** `v{BOT_VER}`"
-        head4 = f"📦 **Module :** `{len(CMD_HELP)}`"
-        head5 = "👨‍💻 **Usage :** `.help <nama module>`"
-        head6 = "Daftar semua perintah tersedia di bawah ini: "
-        head7 = "📌 **Gunakan perintah diatas dengan bijak dan seperlunya, resiko ditanggung pengguna!**"
-
-        string = ""
+        modules = ""
         for index in CMD_HELP:
-            string += f"`{str(index)}` | "
-        string = string[:-2]
+            modules += f"`{str(index)}` • "
+        modules = modules[:-2]
+
+        text = f"""`{BOT_NAME}`
+[REPO](https://github.com/notudope/notubot)  /  [Channel](https://t.me/notudope)  /  [Grup](https://t.me/NOTUBOTS)
+
+😎 **Owner :** __{ALIVE_NAME}__
+🤖 **Version :** `v{BOT_VER}`
+📦 **Module :** `{len(CMD_HELP)}`
+👨‍💻 **Usage :** `.help <nama module>`
+
+Daftar semua module beserta perintah tersedia di bawah ini:
+
+{modules}
+
+📌 **Gunakan perintah dengan bijak dan seperlunya, resiko ditanggung pengguna!**"""
 
         await event.edit("⚡")
         await asyncio.sleep(0.5)
         await event.delete()
-
         helper = await event.client.send_message(
             event.chat_id,
-            f"{head}\
-              \n{head1}\
-              \n\n{head2}\
-              \n{head3}\
-              \n{head4}\
-              \n{head5}\
-              \n\n{head6}\
-              \n\n{string}\
-              \n\n{head7}",
+            text,
             link_preview=False,
         )
-
         await helper.reply(f"\n**Contoh** : Ketik <`.help admin`> Untuk informasi pengunaan.")
         await asyncio.sleep(1000)
         await helper.delete()
