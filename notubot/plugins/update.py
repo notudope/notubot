0662ad18862851300425af75194a9b4109534966
notudope <notudope@gmail.com>
2021-11-03 11:02:54 +0700

    update

--- notubot/plugins/update.py
+++ notubot/plugins/update.py
@@ -57,7 +57,7 @@ async def gen_chlog(repo, diff):
 
 
 async def print_changelogs(event, ac_br, changelog):
-    changelog_str = f"`{__botname__}` **Pembaruan Tersedia Untuk [{ac_br}]:\n\nCHANGELOG:**\n`{changelog}`"
+    changelog_str = f"🔥 **Pembaruan Tersedia Untuk [{ac_br}]:\n\nCHANGELOG:**\n`{changelog}`"
 
     if len(changelog_str) > 4096:
         await event.edit("`Data CHANGELOG terlalu besar, buka file untuk melihatnya.`")
