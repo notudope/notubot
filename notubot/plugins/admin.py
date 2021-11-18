62afd5a7af630f06e59a28b129e05bb982a660cc
notudope <notudope@gmail.com>
2021-11-18 12:57:38 +0700

    update

--- notubot/plugins/admin.py
+++ notubot/plugins/admin.py
@@ -34,6 +34,7 @@ from telethon.tl.types import (
 from telethon.tl.types import ChannelParticipantsAdmins as Admins
 from telethon.tl.types import ChannelParticipantAdmin as Admin
 from telethon.tl.types import ChannelParticipantCreator as Creator
+from telethon.types import TypeChannelParticipantsFilter
 from telethon.utils import get_display_name
 
 from notubot import (
@@ -649,14 +650,15 @@ async def everyone(event):
     mention_slots = 4096 - len(mention_text)
 
     chat = await event.get_chat()
-    async for x in event.client.iter_participants(chat, aggressive=True):
-        if not (x.deleted or x.bot or x.id == bot.uid):
-            mention_text += f"[\u200b](tg://user?id={x.id})"
-            mention_slots -= 1
-            if mention_slots == 0:
-                break
+    async for x in event.client.iter_participants(chat, filter=TypeChannelParticipantsFilter):
+        mention_text += f"[\u200b](tg://user?id={x.id})"
+        mention_slots -= 1
+        if mention_slots == 0:
+            break
 
-    await event.client.send_message(chat, mention_text, reply_to=event.message.reply_to_msg_id)
+    # await event.client.send_message(chat, mention_text, reply_to=event.message.reply_to_msg_id)
+    await event.respond(mention_text, reply_to=event.message.reply_to_msg_id)
+    await event.delete()
 
 
 @bot_cmd(groups_only=True, admins_only=True, pattern="all(?: |$)(.*)|@all(?: |$)(.*)")
