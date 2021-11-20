ea7d84925ee38908b190cfbf1113b009eecf6e42
notudope <notudope@gmail.com>
2021-11-21 05:53:17 +0700

    update

--- notubot/functions/format.py
+++ notubot/functions/format.py
@@ -12,7 +12,7 @@ from telethon.tl.tlobject import TLObject
 from telethon.tl.types import MessageEntityPre
 from telethon.utils import add_surrogate
 
-from notubot.functions.tools import utc_to_local
+from notubot.functions.helper import utc_to_local
 
 
 def parse_pre(text):
